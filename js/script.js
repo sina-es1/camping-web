72e76b65cf47e67d1f55b4b0bc5939f4a23631da
moheb <134a137z@gmail.com>
2024-12-02 19:10:31 +0330

    finish the conections part

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("#btn");
+let menu = document.querySelector("#mobilenavmenu")
+
+btn.addEventListener("toggle",function (menu){
+    menu.classList.toggle("active")
+    
+})
